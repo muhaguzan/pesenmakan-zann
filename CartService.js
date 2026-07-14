42a886ed2aec5623229553399cbbfd416929c561
muhaguzan <mgustifauzan2794@gmail.com>
2026-07-15 01:56:28 +0700

    feat(cart): initialize CartService with basic add method

--- CartService.js
+++ CartService.js
@@ -1,10 +1,13 @@
+// CartService.js
 class CartService {
     constructor() {
         this.cart = [];
     }
-    addToCart(item) {
-        this.cart.push(item);
-        console.log("Item added to cart:", item);
+
+    tambahItem(item) {
+        // BARIS KONFLIK: Aturan harga akan ditulis di sini
+        this.cart.push(item); 
+        console.log("Item berhasil ditambahkan!");
     }
 }
 module.exports = CartService;

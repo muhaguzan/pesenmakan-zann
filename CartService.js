f22a90ec05937213a9d7c836e38be48d187c053b
muhaguzan <mgustifauzan2794@gmail.com>
2026-07-15 02:05:46 +0700

    feat(cart): implement 10% member discount validation

--- CartService.js
+++ CartService.js
@@ -4,10 +4,11 @@ class CartService {
         this.cart = [];
     }
 
-  tambahItem(item) {
+    tambahItem(item) {
         // Aturan Diskon Member 10%
         const hargaFinal = item.harga * 0.9;
         this.cart.push({ ...item, harga: hargaFinal });
         console.log("Item member berhasil ditambahkan dengan diskon 10%!");
     }
+}
 module.exports = CartService;
